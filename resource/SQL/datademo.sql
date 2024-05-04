4f3ef99bc95b9e1e793496599ce1cbf7bbee28cc
cavaldos <khanhnguyen297kn@gmail.com>
2024-05-04 08:43:02 +0700

    clean code

---
+++ resource/SQL/datademo.sql
@@ -0,0 +1,10 @@
+insert into `account` (username, password, status, role, firstname, lastname) values ('admin', 'admin', 1, 'admin', 'admin', 'admin');
+insert into `account` (username, password, status, role, firstname, lastname) values ('employee', 'employee', 1, 'employee', 'employee', 'employee');
+insert into `account` (username, password, status, role, firstname, lastname) values ('employee1', 'employee1', 1, 'employee', 'employee1', 'employee1');
+
+
+INSERT INTO `customer` (`username`, `firstname`, `lastname`) VALUES ('customer1', 'customer1', 'customer1');
+INSERT INTO `customer` (`username`, `firstname`, `lastname`) VALUES ('customer2', 'customer2', 'customer2');
+INSERT INTO `customer` (`username`, `firstname`, `lastname`) VALUES ('customer3', 'customer3', 'customer3');
+INSERT INTO `customer` (`username`, `firstname`, `lastname`) VALUES ('customer4', 'customer4', 'customer 4');
+INSERT INTO `customer` (`username`, `firstname`, `lastname`) VALUES ('customer5', 'customer5', 'customer5');
